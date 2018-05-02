9801b484aa326b29f7a87b0aa65dfb2c3834cbfa
TechQuery <shiy007@qq.com>
2018-05-02 13:50:19 +0800

    [ Add ]  Selecting logic

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,3 @@
 # Month picker
 
-A [Web component](https://www.webcomponents.org/) for picking months by different intervals
+A [Web component](https://www.webcomponents.org/) for picking months by different ranges
--- source/index.js
+++ source/index.js
@@ -11,15 +11,56 @@
 
             shadow.appendChild( template.cloneNode( true ) );
 
+            this.range = 1;
+
             shadow.addEventListener('click',  (event) => {
 
-                Array.from(
-                    event.target.parentNode.children,
-                    (child) => child.classList.remove('active')
-                );
+                const target = event.target;
 
-                event.target.classList.add('active');
+                if ( target.matches('.table-row > ul > li') ) {
+
+                    this.toggleActive( target );
+
+                    if ( target.dataset.range )
+                        this.range = +target.dataset.range;
+
+                } else if ( target.matches('main > span') )
+                    this.toggleActive(target, this.range);
+                else if ( target.matches('nav > span') )
+                    this.switchYear( target );
             });
+        }
+
+        static indexOf(element) {
+
+            var index = 0;
+
+            while (element = element.previousElementSibling)  index++;
+
+            return index;
+        }
+
+        toggleActive(target,  range = 1) {
+
+            const active = MonthPicker.indexOf( target );
+
+            Array.from(target.parentNode.children,  (child, index) => {
+
+                if ((child === target)  ||  (
+                    (index > active)  &&  (index < active + range)
+                ))
+                    child.classList.add('active');
+                else
+                    child.classList.remove('active');
+            });
+        }
+
+        switchYear (target) {
+
+            if ( target.previousElementSibling )
+                target.previousElementSibling.textContent++;
+            else
+                target.nextElementSibling.textContent--;
         }
     }
 
