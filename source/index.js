cf60fdee8c422b8380f2ef1b3bbda7254f1764d0
TechQuery <shiy007@qq.com>
2018-05-03 20:01:26 +0800

    [ Add ]  Method & event of changing value [ Fix ]  Compatibility issue of the float layer

--- source/index.js
+++ source/index.js
@@ -1,8 +1,12 @@
 (() => {  'use strict';
 
-    const _private_ = new WeakMap();
-
-    const template = document.currentScript.previousElementSibling.content;
+    const _private_ = new WeakMap(),
+        selector_map = {
+            '.table-row > ul > li':    'Range',
+            'main > span':             'Month',
+            'nav > span':              'Year'
+        },
+        template = document.currentScript.previousElementSibling.content;
 
 
     class MonthPicker extends HTMLElement {
@@ -48,7 +52,7 @@
 
         set value(value) {
 
-            _private_.get( this ).value = value;
+            _private_.get( this ).value = value = value + '';
 
             value = value.split(',');
 
@@ -56,41 +60,92 @@
                 this.shadowRoot.querySelectorAll('input'),
                 (input, index)  =>  input.value = value[ index ] || ''
             );
+
+            const event = document.createEvent('Event');
+
+            event.initEvent('change', true, false);
+
+            this.shadowRoot.host.dispatchEvent( event );
+        }
+
+        static targetOf(event) {
+
+            const target = event.composedPath ? event.composedPath() : event.path;
+
+            return  (target || '')[0]  ||  event.target;
+        }
+
+        show(visible, event) {
+
+            const layer = this.shadowRoot.querySelector('input[name] + .table-row');
+
+            if (! visible)  return  (! layer.hidden) && (layer.hidden = true);
+
+            const coord = MonthPicker.targetOf( event ).getBoundingClientRect();
+
+            layer.style.left = coord.left;
+
+            layer.style.top = coord.bottom;
+
+            if ( layer.hidden )  layer.hidden = false;
         }
 
         listen() {
 
-            const _this_ = _private_.get( this );
+            const year = this.shadowRoot.querySelector('nav > div'),
+                month = this.shadowRoot.querySelectorAll('main > span');
 
-            const layer = this.shadowRoot.querySelector('input[name] + [hidden]');
+            this.shadowRoot.host.addEventListener(
+                'focus',  this.show.bind(this, true)
+            );
+
+            this.shadowRoot.host.addEventListener(
+                'blur',  this.show.bind(this, false)
+            );
 
             this.shadowRoot.addEventListener('click',  event => {
 
                 const target = event.target;
 
-                if ( target.matches('.table-row > ul > li') )
-                    this.switchRange( target );
-                else if ( target.matches('main > span') )
-                    this.toggleActive(target, _this_.range);
-                else if ( target.matches('nav > span') )
-                    this.switchYear( target );
+                for (let selector  in  selector_map)
+                    if (target.matches( selector ))
+                        this['switch' + selector_map[selector]]( target );
             });
 
-            this.shadowRoot.host.addEventListener(
-                'focus',  () => layer.hidden && (layer.hidden = false)
-            );
+            this.shadowRoot.querySelector('input[name]').addEventListener(
+                'change',  event => {
 
-            this.shadowRoot.host.addEventListener(
-                'blur',  () => (! layer.hidden) && (layer.hidden = true)
+                    const date = new Date( event.target.value );
+
+                    if (date == 'Invalid Date')
+                        return  event.target.select(), event.target.focus();
+
+                    year.textContent = date.getFullYear();
+
+                    this.switchMonth( month[ date.getMonth() ] );
+                }
             );
+        }
+
+        makeDate(month) {
+
+            const year = this.shadowRoot.querySelector('nav > div').textContent;
+
+            month = month  ?  `${year}-${month.padStart(2, 0)}`  :  Date.now();
+
+            return  (new Date( month )).toISOString().slice(0, 7);
         }
 
         connectedCallback() {
 
-            this.shadowRoot.querySelector('.table-row > ul > li').click();
+            this.shadowRoot.querySelector('nav > div').textContent =
+                (new Date()).getFullYear();
 
-            this.value = this.defaultValue ||
-                (new Date()).toISOString().slice(0, 7);
+            this.switchRange(
+                this.shadowRoot.querySelector('.table-row > ul > li')
+            );
+
+            this.value = this.defaultValue || this.makeDate();
         }
 
         static indexOf(element) {
@@ -104,17 +159,38 @@
 
         toggleActive(target,  range = 1) {
 
-            const active = MonthPicker.indexOf( target );
+            const active = MonthPicker.indexOf( target ), list = [ ];
 
             Array.from(target.parentNode.children,  (child, index) => {
 
                 if ((child === target)  ||  (
                     (index > active)  &&  (index < active + range)
-                ))
+                )) {
                     child.classList.add('active');
-                else
+
+                    list.push( child.textContent );
+                } else
                     child.classList.remove('active');
             });
+
+            return list;
+        }
+
+        switchMonth(first) {
+
+            first = first || this.shadowRoot.querySelector('main > .active');
+
+            if (! first)  return;
+
+            const _this_ = _private_.get( this );
+
+            const active = this.toggleActive(first, _this_.range);
+
+            const last = active[1] && active.pop();
+
+            this.value = [
+                this.makeDate( active[0] ),  last && this.makeDate( last )
+            ];
         }
 
         switchRange(target) {
@@ -128,9 +204,7 @@
             this.shadowRoot.querySelectorAll('input')[1].disabled =
                 (_this_.range === 1);
 
-            let first = this.shadowRoot.querySelector('main > .active');
-
-            if (first)  this.toggleActive(first, _this_.range);
+            this.switchMonth();
         }
 
         switchYear (target) {
@@ -139,6 +213,8 @@
                 target.previousElementSibling.textContent++;
             else
                 target.nextElementSibling.textContent--;
+
+            this.switchMonth();
         }
     }
 
