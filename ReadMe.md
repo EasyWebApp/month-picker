92104ff017fbc0ad2ca7066354eaa8a9027f733b
TechQuery <shiy007@qq.com>
2018-05-03 16:38:09 +0800

    [ Add ]  Basic support of Month value [ Add ]  ESLint configuration

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,9 @@
 # Month picker
 
 A [Web component](https://www.webcomponents.org/) for picking months by different ranges
+
+
+
+## Demo
+
+https://techquery.github.io/month-picker/
--- source/index.js
+++ source/index.js
@@ -1,4 +1,6 @@
-(() => {
+(() => {  'use strict';
+
+    const _private_ = new WeakMap();
 
     const template = document.currentScript.previousElementSibling.content;
 
@@ -7,28 +9,88 @@
 
         constructor() {
 
-            const shadow = super().attachShadow({mode: 'open'});
+            _private_.set(super(),  {range: 1});
 
-            shadow.appendChild( template.cloneNode( true ) );
+            this.attachShadow({
+                mode:              'open',
+                delegatesFocus:    true
+            }).appendChild(
+                template.cloneNode( true )
+            );
 
-            this.range = 1;
+            this.listen();
+        }
 
-            shadow.addEventListener('click',  (event) => {
+        static get observedAttributes() {
+
+            return ['value'];
+        }
+
+        attributeChangedCallback(name, oldValue, newValue) {
+
+            this[ name ] = newValue;
+        }
+
+        get range() {
+
+            return  _private_.get( this ).range;
+        }
+
+        get defaultValue() {
+
+            return this.getAttribute('value');
+        }
+
+        get value() {
+
+            return  _private_.get( this ).value;
+        }
+
+        set value(value) {
+
+            _private_.get( this ).value = value;
+
+            value = value.split(',');
+
+            Array.from(
+                this.shadowRoot.querySelectorAll('input'),
+                (input, index)  =>  input.value = value[ index ] || ''
+            );
+        }
+
+        listen() {
+
+            const _this_ = _private_.get( this );
+
+            const layer = this.shadowRoot.querySelector('input[name] + [hidden]');
+
+            this.shadowRoot.addEventListener('click',  event => {
 
                 const target = event.target;
 
-                if ( target.matches('.table-row > ul > li') ) {
-
-                    this.toggleActive( target );
-
-                    if ( target.dataset.range )
-                        this.range = +target.dataset.range;
-
-                } else if ( target.matches('main > span') )
-                    this.toggleActive(target, this.range);
+                if ( target.matches('.table-row > ul > li') )
+                    this.switchRange( target );
+                else if ( target.matches('main > span') )
+                    this.toggleActive(target, _this_.range);
                 else if ( target.matches('nav > span') )
                     this.switchYear( target );
             });
+
+            this.shadowRoot.host.addEventListener(
+                'focus',  () => layer.hidden && (layer.hidden = false)
+            );
+
+            this.shadowRoot.host.addEventListener(
+                'blur',  () => (! layer.hidden) && (layer.hidden = true)
+            );
+        }
+
+        connectedCallback() {
+
+            this.shadowRoot.querySelector('.table-row > ul > li').click();
+
+            this.value = this.defaultValue ||
+                (new Date()).toISOString().slice(0, 7);
         }
 
         static indexOf(element) {
@@ -53,6 +115,22 @@
                 else
                     child.classList.remove('active');
             });
+        }
+
+        switchRange(target) {
+
+            const _this_ = _private_.get( this );
+
+            this.toggleActive(target);
+
+            _this_.range = +target.dataset.range;
+
+            this.shadowRoot.querySelectorAll('input')[1].disabled =
+                (_this_.range === 1);
+
+            let first = this.shadowRoot.querySelector('main > .active');
+
+            if (first)  this.toggleActive(first, _this_.range);
         }
 
         switchYear (target) {
